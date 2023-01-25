e969aeaefe492ca1fbdb0cd2fa8d319347a846a0
maximebrny <bruny.maxime@orange.fr>
2023-01-25 10:55:10 +0100

    finish frontend post

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
+// Import moment.js library
+const moment = require("moment");
+
+// Set the locale to French
+moment.locale("fr");
+
 // Tableau vide du panier
 let cart = [];
+const BACKEND_URL = "http://localhost:3000";
 
 document.querySelector("#btnSearch").addEventListener("click", function () {
   const departure = document.querySelector("#departureInput").value;
   const arrival = document.querySelector("#arrivalInput").value;
   const date = document.querySelector("#dateInput").value;
 
+  if (!departure || !arrival || !date) {
+    return res.json({ error: "Missing required fields" });
+  }
+
   // Fetch the routes of the match with criteres and req.query
-  fetch(`/trips/match?departure=${departure}&arrival=${arrival}&date=${date}`)
+  fetch(
+    `${BACKEND_URL}/trips/match?departure=${departure}&arrival=${arrival}&date=${date}`
+  )
     .then((res) => res.json())
     .then((trips) => {
       document.querySelector("#resultsBox").innerHTML = "";
 
       trips.forEach((trip) => {
         const li = document.createElement("li");
+        li.setAttribute("class", "litrips");
         li.textContent = `${trip.departure} > ${trip.arrival} ${moment(
           trip.time
-        ).format("LT")} ${trip.price}`;
+        ).format("LT")} ${trip.price}€`;
         // Button to book a trip, add the trip in the cart
         const bookBtn = document.createElement("button");
+        bookBtn.setAttribute("class", "bookbtn");
         bookBtn.textContent = "Book";
         bookBtn.addEventListener("click", function () {
           addTripToCart(trip);
@@ -30,28 +45,28 @@ document.querySelector("#btnSearch").addEventListener("click", function () {
     .catch((error) => console.log(error));
 });
 
-// Ajout des trips au panier (cart)
-cart.forEach((trip) => {
-  const li = document.createElement("li");
-  li.textContent = `${trip.departure} to ${trip.arrival} at ${moment(
-    trip.time
-  ).format("LT")} for ${trip.price}`;
-  // Button "Delete"
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
-  // AddEventListener au button delete
-  removeBtn.addEventListener("click", function () {
-    // Delete button
-    removeTripFromCart(trip);
+function addTripToCart(trip) {
+  // Ajout des trips au panier (cart)
+  cart.forEach((trip) => {
+    const li = document.createElement("li");
+    li.textContent = `${trip.departure} to ${trip.arrival} at ${moment(
+      trip.time
+    ).format("LT")} for ${trip.price}`;
+    // Button "Delete"
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    // AddEventListener au button delete
+    removeBtn.addEventListener("click", function () {
+      // Delete button
+      removeTripFromCart(trip);
+    });
+    li.appendChild(removeBtn);
+    document.querySelector("#cart").appendChild(li);
   });
-  li.appendChild(removeBtn);
-  document.querySelector("#cart").appendChild(li);
-});
+}
 
 // Update bookings
 // function updateBookings() {
-// Code to change "isPaid" to true for all bookings in the cart
-// ...
 // }
 
 // Delete trip
@@ -68,6 +83,4 @@ function removeTripFromCart(trip) {
 
 // Remove booking from "bookings" collection
 // function removeBooking(trip) {
-// Code to remove booking from "bookings" collection
-// ...
 // }
